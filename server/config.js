f42f71afabb9c0bb9623fa34e26697309a328d22
sasham43 <sashasemail@gmail.com>
2016-08-19 10:35:21 -0500

    add twitter codes

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'dANG785bOsODe81fqIRaL7zOy',
+    consumer_secret: 'Hn4UpKsD2hkPC6F895GJ16evFmXTqlWbZPVZOS8H7WbuZp0kYR',
+    access_token_key: '750890864600375296-RfG5uzMfRB7TEuC1s8LAOErsX4NvdVZ',
+    access_token_secret: '	0TLTjzC0de4gn4vspwSSBcV23GRNQZDA5Ak7XNTTj6ApV'
+  }
+}
---
+++ server/server.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+  exphbs = require('express-handlebars'),
+  http = require('http'),
+  mongoose = require('mongoose'),
+  twitter = require('ntwitter'),
+  routes = require('./routes'),
+  config = require('./config'),
+  streamHandler = require('./utils/streamHandler');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// set up handlebars as the templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// disable etags on responses
+app.disable('etag');
+
+// connect to db
+mongoose.connect('mongodb://localhost/react-tweets');
+
+// create new twitter instance
+var twit = new twitter(config.twitter);
+
+// page routes
+app.get('page/:page/:skip', routes.page);
+
+app.use("/", express.static(__dirname + "/public/"));
+
+// start up server
+var server = http.createServer(app).listen(port, function(){
+  console.log('Twitter server listening on port ' + port);
+});
